0fd24a14e2bde9da415e04e0c7f42a0cc655200a
joy <le821227@gmail.com>
2019-05-29 20:51:30 +0800

    切割版面

--- backofficeTest/ViewController.swift
+++ backofficeTest/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myImageView.animationImages = [UIImage(named: "i1")!, UIImage(named: "i2")!, UIImage(named: "i3")!,UIImage(named: "i4")!, UIImage(named: "i5")!, UIImage(named: "i6")!, UIImage(named: "i7")!, UIImage(named: "i8")!, UIImage(named: "i9.png")!, UIImage(named: "i10")!, UIImage(named: "i11")!, UIImage(named: "i12")!, UIImage(named: "i13")!]
+        myImageView.animationDuration = 1.5
+        myImageView.animationRepeatCount = 0
+        myImageView.startAnimating()
     }
 
 
+    
+    
 }
 
